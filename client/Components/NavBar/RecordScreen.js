4a4c7da07c7df057754c7303959ab04e1ab92a2d
Ryan See <ryanjosephsee@gmail.com>
2023-08-30 20:41:52 -0700

    adjusted record screen

--- client/Components/NavBar/RecordScreen.js
+++ client/Components/NavBar/RecordScreen.js
@@ -8,43 +8,52 @@ export default function RecordScreen() {
     const [route, setRoute] = useState([]);
     const [tracking, setTracking] = useState(false);
     const [distance, setDistance] = useState(0);
+    let watchId;
 
-    const startTracking = () => {
+    const startTracking = async () => {
         setTracking(true);
 
-        // Watch position changes
-        Geolocation.watchPosition(
+        // Request permission to access location
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            console.error('Permission to access location was denied');
+            return;
+        }
+
+        watchId = await Location.watchPositionAsync(
+            {
+                accuracy: Location.Accuracy.High,
+                distanceInterval: 10, // updates every 10 meters
+            },
             position => {
                 const { latitude, longitude } = position.coords;
 
                 const newRoute = [...route, { latitude, longitude }];
                 setRoute(newRoute);
 
-                // Calculate distance if you have previous position (you may want to use a more sophisticated approach for accurate measurement)
+                // Calculate distance if you have previous position
                 if (location) {
                     const delta = getDistanceBetweenPoints(location, position.coords);
                     setDistance(distance + delta);
                 }
 
                 setLocation(position.coords);
-            },
-            error => console.log(error),
-            {
-                enableHighAccuracy: true,
-                distanceFilter: 10, // updates every 10 meters
             }
         );
     };
 
     const stopTracking = () => {
         setTracking(false);
-        Geolocation.stopObserving();
+        if (watchId) {
+            watchId.remove();
+        }
     };
 
     const getDistanceBetweenPoints = (point1, point2) => {
-        // A basic function to get distance between two lat-long points (can use a library for more accurate calculations)
-        // This is a mock implementation
-        return Math.sqrt(Math.pow(point2.latitude - point1.latitude, 2) + Math.pow(point2.longitude - point1.longitude, 2));
+        // A basic function to get distance between two lat-long points 
+        // (can use a library for more accurate calculations)
+        return Math.sqrt(Math.pow(point2.latitude - point1.latitude, 2) + 
+                         Math.pow(point2.longitude - point1.longitude, 2));
     };
 
     return (
@@ -62,9 +71,16 @@ export default function RecordScreen() {
             >
                 {route.length > 0 && <Polyline coordinates={route} />}
             </MapView>
-            <Button title={tracking ? "Stop Tracking" : "Start Tracking"} onPress={tracking ? stopTracking : startTracking} />
-            <Text>Distance Travelled: {distance.toFixed(2)} km</Text>
-            <Text>Estimated Carbon Saved: {(distance * 0.2).toFixed(2)} kg</Text> {/* Assuming 0.2 kg saved per km. Adjust based on your data */}
+            <Button 
+                title={tracking ? "Stop Tracking" : "Start Tracking"} 
+                onPress={() => { tracking ? stopTracking() : startTracking() }} 
+            />
+            {distance !== 0 && (
+                <>
+                    <Text>Distance Travelled: {distance.toFixed(2)} km</Text>
+                    <Text>Estimated Carbon Saved: {(distance * 0.2).toFixed(2)} kg</Text>
+                </>
+            )}
         </View>
     );
 }

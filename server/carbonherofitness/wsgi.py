13f2b055cdbeddf58654bfd244a395c6384d6872
Ryan See <ryanjosephsee@gmail.com>
2023-10-02 16:24:41 -0700

    still errors

--- server/carbonherofitness/wsgi.py
+++ server/carbonherofitness/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for carbonhero_backend project.
+WSGI config for carbonherofitness project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carbonhero_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carbonherofitness.settings')
 
 application = get_wsgi_application()
